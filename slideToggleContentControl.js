4804c69a714c0a0f29e11a01f2beae9a856b1534
Celadora <Celadora@users.noreply.github.com>
2016-04-12 22:57:43 -0700

    updated to bind scope.toggled instead of attr.expanded
    The original author did most of the ground work, but for my needs I wanted to bind the toggled state to a parent scope. I modified the directive names to reflect their roles in the way I saw most accurate. Added a timeout function to slide the content in or out based on the value passed to the directive through scope.toggled.  I think this is a more "angular" way, since now I can bind this value in two directions; this allows me to save the clients toggled state. I also arranged the sliding code into an encapsulated function to improve readability and code reuse.

--- slideToggleContentControl.js
+++ slideToggleContentControl.js
@@ -1,7 +1,7 @@
-angular.module('angularSlideables', [])
-.directive('slideable', function () {
+angular.module('slideToggleContentControl', [])
+.directive('slideToggleContent', function () {
     return {
-        restrict:'C',
+        restrict:'A',
         compile: function (element, attr) {
             // wrap tag
             var contents = element.html();
@@ -9,7 +9,7 @@ angular.module('angularSlideables', [])
 
             return function postLink(scope, element, attrs) {
                 // default properties
-                attrs.duration = (!attrs.duration) ? '1s' : attrs.duration;
+                attrs.duration = (!attrs.duration) ? '0.5s' : attrs.duration;
                 attrs.easing = (!attrs.easing) ? 'ease-in-out' : attrs.easing;
                 element.css({
                     'overflow': 'hidden',
@@ -22,28 +22,51 @@ angular.module('angularSlideables', [])
         }
     };
 })
-.directive('slideToggle', function() {
+.directive('slideToggleControl', function($timeout) {
     return {
         restrict: 'A',
+		scope: {'toggled': '=?'},
         link: function(scope, element, attrs) {
             var target, content;
+			// scoped boolean variable for when the slide is toggled
+            scope.toggled = scope.toggled || false;
             
-            attrs.expanded = false;
-            
-            element.bind('click', function() {
-                if (!target) target = document.querySelector(attrs.slideToggle);
+			// slides the slide based on the value of toggle
+			function slide(toggled)
+			{
+				if (!target) target = document.querySelector(attrs.slideToggleControl);
                 if (!content) content = target.querySelector('.slideable_content');
                 
-                if(!attrs.expanded) {
+                if(toggled) {
                     content.style.border = '1px solid rgba(0,0,0,0)';
-                    var y = content.clientHeight;
                     content.style.border = 0;
-                    target.style.height = y + 'px';
+                    target.style.height = content.clientHeight + 'px';
                 } else {
                     target.style.height = '0px';
                 }
-                attrs.expanded = !attrs.expanded;
+			}
+			
+			// toggles the slide
+			function toggle()
+			{
+				// toggle boolean first
+				scope.toggled = !scope.toggled;
+				
+				// call the slide method
+				slide(scope.toggled);
+			}
+			
+			// handle when the control is clicked
+            element.bind('click', function() {
+                toggle();
             });
+			
+			// initialize the slide after the directive has compiled
+			$timeout(function(){
+				// slide content in or out based on scope.toggled
+				slide(scope.toggled)
+			}, 1);
+			
         }
     }
 });
